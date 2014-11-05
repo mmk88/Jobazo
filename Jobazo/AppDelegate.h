70fb5516194f23eaa0aa9d08362f7eb2a0413b68
Munaf Kachwala <munaf@Munafs-MacBook-Pro.local>
2014-11-04 20:08:50 -0500

    Initial Commit

---
+++ Jobazo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jobazo
+//
+//  Created by Munaf Kachwala on 11/4/14.
+//  Copyright (c) 2014 Munaf M. Kachwala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jobazo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jobazo
+//
+//  Created by Munaf Kachwala on 11/4/14.
+//  Copyright (c) 2014 Munaf M. Kachwala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
